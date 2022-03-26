feda1568cda6f60333a310345d2e47669af43e43
sansoj <jonasans2@live.com.ar>
2022-03-26 00:29:55 -0300

    js

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 // DO WHILE con un IF
 // Adivinanza de mi nombre
 
-let pass = "Jonathan";
-let dato = (prompt("Adivina mi nombre! la primera letra es J"));
+let nombre = "Jonathan";
+let pregunta = (prompt("Adivina mi nombre! la primera letra es J"));
 let intentos = 3;
 
-while (dato != pass) {
+while (pregunta != nombre) {
   if (intentos > 0) {
+    alert(`Todavia no adivinaste, mi nombre termina con -n-, te quedan ${intentos} intentos`);
+    pregunta = (prompt("Adivina mi nombre..."));
     intentos--;
-    alert(`Todavia no adivinaste, mi nombre termina con n, te quedan ${intentos} intentos`);
-    dato = (prompt("Adivina mi nombre..."));
+    alert(`Todavia no adivinaste, la segunda letra es -o-, te quedan ${intentos} intentos`);
+    pregunta = (prompt("Adivina mi nombre..."));
     intentos--;
-    alert(`Todavia no adivinaste, la segunda letra es o, te quedan ${intentos} intentos`);
-    dato = (prompt("Adivina mi nombre..."));
+    alert(`Todavia no adivinaste, lleva -h- intermedia, te quedan ${intentos} intentos.`);
+    pregunta = (prompt("Adivina mi nombre..."));
     intentos--;
-    alert(`Todavia no adivinaste, te quedan ${intentos} intentos pero te regalo un intentos mas,  la segunda letra es o.`);
-    dato = (prompt("Adivina mi nombre..."));
   }
   else {
     alert("se acabaron los intentos");
